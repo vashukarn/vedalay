87fe319e808231613e64feec97292e94355ab9c5
Jatin Singh <jd75177@gmail.com>
2021-02-28 04:34:42 +0530

    jd

--- resources/views/layouts/price-table.blade.php
+++ resources/views/layouts/price-table.blade.php
@@ -668,9 +668,9 @@
     </div>
 
 
-	<script type='text/javascript' src='js/jquery/jquery.js'></script>
-    <script type='text/javascript' src='js/jquery/jquery-migrate.min.js'></script>
-    <script type='text/javascript' src='plugins/goodlayers-core/plugins/combine/script.js'></script>
+	<script type='text/javascript' src="{{ asset('assets/js/jquery/jquery.js')}}"></script>
+    <script type='text/javascript' src="{{ asset('assets/js/jquery/jquery-migrate.min.js')}}"></script>
+    <script type='text/javascript' src="{{ asset('assets/plugins/goodlayers-core/plugins/combine/script.js')}}"></script>
     <script type='text/javascript'>
         var gdlr_core_pbf = {
             "admin": "",
@@ -681,13 +681,13 @@
             "ajax_url": "#"
         };
     </script>
-    <script type='text/javascript' src='plugins/goodlayers-core/include/js/page-builder.js'></script>
-    <script type='text/javascript' src='js/jquery/ui/effect.min.js'></script>
+    <script type='text/javascript' src="{{ asset('assets/plugins/goodlayers-core/include/js/page-builder.js')}}"></script>
+    <script type='text/javascript' src="{{ asset('assets/js/jquery/ui/effect.min.js')}}"></script>
     <script type='text/javascript'>
         var kingster_script_core = {
             "home_url": "index.html"
         };
     </script>
-    <script type='text/javascript' src='js/plugins.min.js'></script>
+    <script type='text/javascript' src="{{ asset('assets/js/plugins.min.js')}}"></script>
 </body>
 </html>
