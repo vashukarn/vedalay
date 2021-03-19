3fa4fa9b61d00a6ae316dbf81d99b88433f8b452
Vijay Karn <vijay@nectardigit.com>
2021-03-20 01:43:58 +0530

    validation by soni

--- public/custom/inventory.js
+++ public/custom/inventory.js
@@ -1,35 +1,33 @@
-$(function() {
+$(function () {
     $("form[name='inventory_form']").validate({
         rules: {
             item_id: "required",
-                quantity: {
-                    required: true,
-                    digits: true,
-                    min: 1,
-                },
-                total_price: {
-                    required: true,
-                    digits: true,
-                    min: 1,
-                },
+            quantity: {
+                required: true,
+                digits: true,
+                min: 1,
             },
-            messages: {
-                item_id: "Please select product",
-                quantity: {
-                    required: "Quantity is required",
-                    digits: " Quantity cannot be a negative value",
-                    min: "Value cannot be less than one",
-                },
-                total_price: {
-                    required: "Total Price is Required",
-                    digits: "Price cannot be a negative value",
-                    min: "Value cannot be less than one",
-                },
-
-                
+            total_price: {
+                required: true,
+                digits: true,
+                min: 1,
+            },
         },
-        submitHandler: function(form) {
+        messages: {
+            item_id: "Please select product",
+            quantity: {
+                required: "Quantity is required",
+                digits: " Quantity cannot be a negative value",
+                min: "Value cannot be less than one",
+            },
+            total_price: {
+                required: "Total Price is Required",
+                digits: "Price cannot be a negative value",
+                min: "Value cannot be less than one",
+            },
+        },
+        submitHandler: function (form) {
             form.submit();
-        }
+        },
     });
 });
