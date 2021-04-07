8f4c59d5b2eccbff4a885762a1414a6e025fcf3c
Vijay Karn <vijay@nectardigit.com>
2021-04-07 23:13:26 +0530

    stable version

--- resources/views/admin/section/header.blade.php
+++ resources/views/admin/section/header.blade.php
@@ -4,13 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('title',env('APP_NAME'))</title>
     @csrf
-
     <link rel="shortcut icon" href="{{ asset('img/AdminLTELogo.png') }}" type="image/png" />
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
     @stack('styles')
-    @laravelPWA
 </head>
 
 <body class="hold-transition sidebar-mini">
